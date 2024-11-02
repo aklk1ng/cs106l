c93428cad593e9f4ab66db4fd30a294e3e9f0776
aklk1ng <2628084745@qq.com>
2024-11-02 13:15:04 +0800

    assign 5

--- assign5/user.h
+++ assign5/user.h
@@ -14,11 +14,14 @@ public:
   std::string get_name() const;
   size_t size() const;
   void set_friend(size_t index, const std::string& name);
-
-  /** 
-   * STUDENT TODO:
-   * Your custom operators and special member functions will go here!
-   */
+  friend std::ostream &operator<<(std::ostream &out, const User &user);
+  ~User();
+  User(const User &user);
+  User &operator=(const User &user);
+  User(User &&user) = delete;
+  User &operator=(User &&user) = delete;
+  User &operator+=(User &other);
+  bool operator<(const User &other) const;
 
 private:
   std::string _name;
